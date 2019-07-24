b5d0ff27e33f1b763084e907eedc17073b108e80
Trang Nguyen <ttn2127@columbia.edu>
2019-07-24 15:47:51 -0400

    added form but still buggy

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {useState} from "react";
+import Form from "../components/form";
+
+
+function App() {
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
